25e5a4f3b4763cedffb4b3414ef37940b846ae73
famolah1717 <famolah17@gmail.com>
2023-11-22 21:25:07 +0700

    init

---
+++ resources/views/page/admin/tutorial/indexTutorial.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.base_admin.base_dashboard')
+@section('judul', 'List Tutorial')
+@section('script_head')
+
+<link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+
+@endsection
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Data Akun</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item">
+                        <a href="{{ route('home') }}">Beranda</a>
+                    </li>
+                    <li class="breadcrumb-item active">Akun</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"></h3>
+            <div class="card-tools">
+                <button
+                    type="button"
+                    class="btn btn-tool"
+                    data-card-widget="collapse"
+                    title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-tool"
+                    data-card-widget="remove"
+                    title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body p-0" style="margin: 20px">
+            <table
+                id="previewTutorial"
+                class="table table-striped table-bordered display"
+                style="width:100%">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>User</th>
+                        <th>Judul Tutorial</th>
+                        <th>Deskripsi</th>
+                        <th>Bahan</th>
+                        <th>Alat</th>
+                        <th>Langkah Tutorial</th>
+                        <th>Foto</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+
+</section>
+@endsection @section('script_footer')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#previewTutorial').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: '{!! route('tutorial.dataTutorial') !!}',
+            columns: [
+                { data: 'id', name: 'id' },
+                { data: 'user_name', name: 'user.name' },
+                { data: 'judul_tutorial', name: 'judul_tutorial' },
+                { data: 'deskripsi', name: 'deskripsi' },
+                { data: 'bahan', name: 'bahan' },
+                { data: 'alat', name: 'alat' },
+                { data: 'langkah_tutorial', name: 'langkah_tutorial' },
+                { data: 'foto', name: 'foto' },
+                { data: 'action', name: 'action', orderable: false, searchable: false }
+            ]
+        });
+    });
+</script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\TutorialController;
+use App\Http\Controllers\AkunController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['prefix' => 'dashboard/admin'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/', [HomeController::class, 'profile'])->name('profile');
+        Route::post('update', [HomeController::class, 'updateprofile'])->name('profile.update');
+    });
+
+    Route::controller(AkunController::class)
+        ->prefix('akun')
+        ->as('akun.')
+        ->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('showdata', 'dataTable')->name('dataTable');
+            Route::match(['get','post'],'tambah', 'tambahAkun')->name('add');
+            Route::match(['get','post'],'{id}/ubah', 'ubahAkun')->name('edit');
+            Route::delete('{id}/hapus', 'hapusAkun')->name('delete');
+            Route::get('/getUserId', 'getUserId')->name('getUserId');
+        });
+    Route::controller(TutorialController::class)
+        ->prefix('tutorial')
+        ->as('tutorial.')
+        ->group(function (){
+            Route::get('tutorial', 'index')->name('index');
+            Route::get('tutorial/editPage', 'editTutorialPage')->name('editPage');
+            Route::get('tutorialDetail/{id}', [TutorialController::class, 'showDetail'])->name('tutorialDetail');
+            Route::get('tutorial/dataTutorial', [TutorialController::class, 'getDataTutorial'])->name('dataTutorial');
+            Route::get('/viewTambah', 'addTutorialPage')->name('tambah');
+            Route::post('tambahTutorial', 'store')->name('store');
+            Route::post('{id}/edit', 'editTutorial')->name('edit');
+            Route::delete('{id}/delete', 'deleteTutorial')->name('delete');
+        });
+
+        
+});
